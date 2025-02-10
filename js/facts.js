1cf73eade2cafc058ed586b085911b5e017ca442
RostislavJirak <rosta1013@gmail.com>
2025-02-10 15:56:54 +0100

    Force

--- js/facts.js
+++ js/facts.js
@@ -34,4 +34,42 @@ document.addEventListener("DOMContentLoaded", function () {
             updateCounter();
         });
     }
+
+    // Animace obrázků
+    let animationIntervals = {}; // Ukládá intervaly pro každý box
+
+    // Spuštění animace při najetí myší
+    function startAnimation(element) {
+        const img = element.querySelector(".icon-animation");
+        const frames = 18; // Počet snímků
+        const framePath = "img/facts/build-icon/build-icon"; // Cesta k obrázkům
+        let currentFrame = 1;
+
+        animationIntervals[element] = setInterval(() => {
+            img.src = `${framePath}${currentFrame}.png`; // Změna obrázku
+            currentFrame++;
+
+            if (currentFrame > frames) {
+                currentFrame = 1; // Restart animace
+            }
+        }, 50); // Rychlejší přepínání snímků (50 ms = 20 FPS)
+    }
+
+    // Zastavení animace při odjetí myší
+    function stopAnimation(element) {
+        clearInterval(animationIntervals[element]); // Zastavení animace
+        const img = element.querySelector(".icon-animation");
+        img.src = "img/facts/build-icon/build-icon1.png"; // Vrácení na první snímek
+    }
+
+    // Přidání listenerů pro hover na všechny boxy
+    const statsBoxes = document.querySelectorAll(".stats-box");
+    statsBoxes.forEach((box) => {
+        box.addEventListener("mouseover", function () {
+            startAnimation(box);
+        });
+        box.addEventListener("mouseout", function () {
+            stopAnimation(box);
+        });
+    });
 });
